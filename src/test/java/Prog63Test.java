fc8cae2485191e6a997b152674bf584d5f571df3
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-10 11:27:40 +0900

    Update Prog63Test.java

--- src/test/java/Prog63Test.java
+++ src/test/java/Prog63Test.java
@@ -1,99 +1,114 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import java.io.*;
-
+/**
+ * @version (20220510)
+ **/
 public class Prog63Test {
+    InputStream originalIn;
+    PrintStream originalOut;
+    ByteArrayOutputStream bos;
+    StandardInputStream in;
+
+    @BeforeEach
+    void before() {
+        //back up binding
+        originalIn  = System.in;
+        originalOut = System.out;
+        //modify binding
+        bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        
+        in = new StandardInputStream();
+        System.setIn(in);
+    }
+    
+    @AfterEach
+    void after() {
+       System.setOut(originalOut);
+       System.setIn(originalIn);
+    }
 
     @Test
     public void testTokenizedResults()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
-        StandardInputStream in = new StandardInputStream();
-        System.setIn(in);
-
         // action
         in.inputln("The tests are currently failing because of an output mismatch."); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog63.main(null);
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals( "mismatch.", prints[prints.length - 1]);
-        assertEquals( "output", prints[prints.length - 2]);
-        assertEquals( "an", prints[prints.length - 3]);
-        assertEquals( "of", prints[prints.length - 4]);
-        assertEquals( "because", prints[prints.length - 5]);
-        assertEquals( "failing", prints[prints.length - 6]);
-        assertEquals( "currently", prints[prints.length - 7]);
-        assertEquals( "are", prints[prints.length - 8]);
-        assertEquals( "tests", prints[prints.length - 9]);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        String[] prints = bos.toString().split(System.lineSeparator());
+        try {
+            assertEquals( "mismatch.", prints[prints.length - 1]);
+            assertEquals( "output", prints[prints.length - 2]);
+            assertEquals( "an", prints[prints.length - 3]);
+            assertEquals( "of", prints[prints.length - 4]);
+            assertEquals( "because", prints[prints.length - 5]);
+            assertEquals( "failing", prints[prints.length - 6]);
+            assertEquals( "currently", prints[prints.length - 7]);
+            assertEquals( "are", prints[prints.length - 8]);
+            assertEquals( "tests", prints[prints.length - 9]);
+        } catch (AssertionError err) {
+            after();
+            AssertionError asErr = new AssertionError("適切なAPIを選んでいない または 改行の仕方が不正です!");
+            throw asErr;     
+        }
     }
 
     @Test
     public void testMultipleSpaces()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
-        StandardInputStream in = new StandardInputStream();
-        System.setIn(in);
-
         // action
         in.inputln("The   tests   are   currently   failing   because   of   an   output   mismatch."); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog63.main(null);
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals( "mismatch.", prints[prints.length - 1]);
-        assertEquals( "output", prints[prints.length - 2]);
-        assertEquals( "an", prints[prints.length - 3]);
-        assertEquals( "of", prints[prints.length - 4]);
-        assertEquals( "because", prints[prints.length - 5]);
-        assertEquals( "failing", prints[prints.length - 6]);
-        assertEquals( "currently", prints[prints.length - 7]);
-        assertEquals( "are", prints[prints.length - 8]);
-        assertEquals( "tests", prints[prints.length - 9]);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        try {
+            String[] prints = bos.toString().split(System.lineSeparator());
+            assertEquals( "mismatch.", prints[prints.length - 1]);
+            assertEquals( "output", prints[prints.length - 2]);
+            assertEquals( "an", prints[prints.length - 3]);
+            assertEquals( "of", prints[prints.length - 4]);
+            assertEquals( "because", prints[prints.length - 5]);
+            assertEquals( "failing", prints[prints.length - 6]);
+            assertEquals( "currently", prints[prints.length - 7]);
+            assertEquals( "are", prints[prints.length - 8]);
+            assertEquals( "tests", prints[prints.length - 9]);
+        } catch (AssertionError err) {
+            after();
+            AssertionError asErr = new AssertionError("適切なAPIを選んでいない または 改行の仕方が不正です!");
+            throw asErr;     
+        }
     }
 
     @Test
     public void testTab()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
-        StandardInputStream in = new StandardInputStream();
-        System.setIn(in);
-
         // action
         in.inputln("The\ttests\tare\tcurrently\tfailing\tbecause\tof\tan\toutput\tmismatch."); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog63.main(null);
 
         // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals( "mismatch.", prints[prints.length - 1]);
-        assertEquals( "output", prints[prints.length - 2]);
-        assertEquals( "an", prints[prints.length - 3]);
-        assertEquals( "of", prints[prints.length - 4]);
-        assertEquals( "because", prints[prints.length - 5]);
-        assertEquals( "failing", prints[prints.length - 6]);
-        assertEquals( "currently", prints[prints.length - 7]);
-        assertEquals( "are", prints[prints.length - 8]);
-        assertEquals( "tests", prints[prints.length - 9]);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        try {
+            String[] prints = bos.toString().split(System.lineSeparator());
+            assertEquals( "mismatch.", prints[prints.length - 1]);
+            assertEquals( "output", prints[prints.length - 2]);
+            assertEquals( "an", prints[prints.length - 3]);
+            assertEquals( "of", prints[prints.length - 4]);
+            assertEquals( "because", prints[prints.length - 5]);
+            assertEquals( "failing", prints[prints.length - 6]);
+            assertEquals( "currently", prints[prints.length - 7]);
+            assertEquals( "are", prints[prints.length - 8]);
+            assertEquals( "tests", prints[prints.length - 9]);
+        } catch (AssertionError err) {
+            after();
+              AssertionError asErr = new AssertionError("適切なAPIを選んでいない または 改行の仕方が不正です!");
+            throw asErr;   
+        }
     }
 }
